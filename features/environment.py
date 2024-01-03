542424283e724b8299b9b2ff20cfd1ae136b498e
Sehij Aulakh <sehij@Sehijs-MacBook-Pro.local>
2024-01-02 23:09:46 -0800

    cap-302

--- features/environment.py
+++ features/environment.py
@@ -1,17 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 from app.application import Application
 from selenium.webdriver.support.wait import WebDriverWait
 
 
-def browser_init(context):
+def browser_init(context,scenario_name ):
     """
     :param context: Behave context
     """
-    driver_path = ChromeDriverManager().install()
-    service = Service(driver_path)
-    context.driver = webdriver.Chrome(service=service)
+    #driver_path = ChromeDriverManager().install()
+    #service = Service(driver_path)
+    #context.driver = webdriver.Chrome(service=service)
 
     ### HEADLESS MODE ####
     #options = webdriver.ChromeOptions()
@@ -26,6 +27,21 @@ def browser_init(context):
     # service = Service(executable_path='/Users/Sehij/Downloads/geckodriver')
     # context.driver = webdriver.Firefox(service=service)
 
+    ### BROWSERSTACK ###
+    bs_user = 'sehijbiraulakh_x6Arzh'
+    bs_key = '5Bx4NpwsNR71xDPyyELD'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+
+    options = Options()
+    bstack_options = {
+         'os': 'OS X',
+         'osVersion': 'Big Sur',
+         'browserName': 'Chrome',
+         'sessionName': scenario_name
+     }
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
+
     context.driver.wait = WebDriverWait(context.driver, 15)
 
     context.driver.maximize_window()
@@ -34,7 +50,7 @@ def browser_init(context):
     context.driver.implicitly_wait(4)
     context.app = Application(context.driver)
 
- ### HEADLESS MODE ####
+    ### HEADLESS MODE ####
     # options = webdriver.ChromeOptions()
     # options.add_argument('headless')
     # service = Service(ChromeDriverManager().install())
@@ -43,9 +59,10 @@ def browser_init(context):
     #     service=service
     # )
 
+
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    browser_init(context,scenario.name)
 
 
 def before_step(context, step):
